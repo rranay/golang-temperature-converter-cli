a6f3ed25373ab76d2d3a26f95432ec57ee243df0
Rex Ranay <rranay@gmail.com>
2020-03-18 22:46:26 +0800

    Completed module

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ func main() {
 	for {
 		if len(os.Args) != 2 {
 			printError(errInvalidArguments)
+			originUnit = strings.ToUpper(os.Args[1])
 		}
 
-		originUnit = strings.ToUpper(os.Args[1])
 
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
